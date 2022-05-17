bd6fde4dab2f08f40e7c2c5ddf55be1c1a0c4701
Lars <lars.eriksson.mail@gmail.com>
2022-05-17 18:28:18 +0200

    Added a search api call.

--- main.py
+++ main.py
@@ -66,6 +66,27 @@ def all_cafes():
 
     return jsonify(cafe_list)
 
+
+@app.route("/search")
+def search():
+    #search for the location via call
+    place = request.args.get("loc")
+    cafe = db.session.query(Cafe).filter_by(location=place).first()
+    print(cafe)
+    if cafe:
+        return jsonify(name=cafe.name,
+            map_url=cafe.map_url,
+            img_url=cafe.img_url,
+            location=cafe.location,
+            seats=cafe.seats,
+            has_toilet=cafe.has_toilet,
+            has_wifi=cafe.has_wifi,
+            has_sockets=cafe.has_sockets,
+            can_take_calls=cafe.can_take_calls,
+            coffee_price=cafe.coffee_price)
+    else:
+        return jsonify(error={"Not found": "No cafe found"})
+
 ## HTTP GET - Read Record
 
 ## HTTP POST - Create Record
